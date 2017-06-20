903c80b2c695068d446bd634a13cd4abcf8c04a6
thehump@gmail.com <asdasd>
2017-06-20 14:06:03 -0300

    4th

--- src/jshell2.java
+++ src/jshell2.java
@@ -9,22 +9,26 @@ import com.ibm.as400.access.AS400;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class jshell2 extends Object {
 
 
-    public static void main(){
-        String commandString = " ";
+    public static void main() throws IOException {
+         System.out.println("jshell2 started");
+        String commandString = "crtlib hacktest8";
         AS400 as400 = new AS400();
         CommandCall command = new CommandCall(as400);
+
         File f = new File("ftpinput.txt");
 
         try{
             FileReader fr = new FileReader(f);
             BufferedReader br = new BufferedReader(fr);
             while(br.ready()){
-                System.out.println(br.readLine());
                 commandString = br.readLine();
+                System.out.println(br.readLine());
+
             }
         }catch(Exception e){
             System.out.println(e);
@@ -35,9 +39,9 @@ public class jshell2 extends Object {
 
         try{
             if (command.run(commandString))
-                System.out.println("jshell successful");
+                System.out.println("jshell2 successful");
             else
-                System.out.println("jshell failed");
+                System.out.println("jshell2 failed");
 
             AS400Message[] messagelist = command.getMessageList();
 
