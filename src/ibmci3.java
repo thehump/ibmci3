1e7caacfe0ceb7f94f890be6ea7f498bb31150e6
thehump@gmail.com <asdasd>
2017-07-05 12:17:38 -0300

    4th

--- src/ibmci3.java
+++ src/ibmci3.java
@@ -1,8 +1,11 @@
+/*  ibmci3.java client program
+David Stec
+Version 1.0, 2017
+ */
+
+
 import com.ibm.as400.access.AS400;
 import com.ibm.as400.access.AS400FTP;
-import com.ibm.as400.access.CommandCall;
-import org.apache.commons.net.ftp.FTPClient;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -19,21 +22,14 @@ public class ibmci3 extends JFrame implements ActionListener {
     JButton btnProcessJshl;
     JButton btnConnect;
     JButton btnExit;
-
     JTextArea txtS;
     JScrollPane scrollS;
     String newline = "\n";
 
-    CommandCall command;
-    public AS400FTP   ftp;
-    public AS400 system;
-
-
     public void actionPerformed(ActionEvent e) {
         AS400 system = new AS400();
         AS400FTP   ftp    = new AS400FTP(system);
-
-
+        //Initiate FTP Connection
         if (e.getSource().equals(btnConnect)) {
               try {
                 connectFTP(ftp);
@@ -43,6 +39,7 @@ public class ibmci3 extends JFrame implements ActionListener {
                 e1.printStackTrace();
             }
         }
+        //Process Display Command
         if (e.getSource().equals(btnProcess)) {
 
 
@@ -54,6 +51,7 @@ public class ibmci3 extends JFrame implements ActionListener {
                 e1.printStackTrace();
             }
         }
+        //Process Jshell Command
         if (e.getSource().equals(btnProcessJshl)) {
 
 
@@ -65,6 +63,7 @@ public class ibmci3 extends JFrame implements ActionListener {
                 e1.printStackTrace();
             }
         }
+        //Delete and Disconnect
         if (e.getSource().equals(btnExit)) {
             try {
                 exitFTP(ftp);
@@ -76,13 +75,10 @@ public class ibmci3 extends JFrame implements ActionListener {
         }
     }
 
+    ////Initiate FTP Connection
     public void connectFTP(AS400FTP ftp) throws UnknownHostException, IOException {
-
- //       AS400 system = new AS400();
- //       AS400FTP   ftp    = new AS400FTP(system);
-
+        //upload and setup required server side programs
         ftp.connect();
-        //System.out.println(ftp.getLastMessage());
         txtS.setText(ftp.getLastMessage());
         ftp.issueCommand("RCMD CRTLIB HACK400B");
         txtS.append(newline + ftp.getLastMessage());
@@ -93,14 +89,10 @@ public class ibmci3 extends JFrame implements ActionListener {
         ftp.put("hack400b.savf", "/qsys.lib/hack400b.lib/hack400b.file");
         txtS.append(newline + ftp.getLastMessage());
         ftp.issueCommand("RCMD RSTOBJ OBJ(*ALL) SAVLIB(HACK400Z) DEV(*SAVF) SAVF(HACK400B/HACK400B) MBROPT(*ALL) ALWOBJDIF(*ALL) RSTLIB(HACK400B) ");
- //       txtS.append(newline + ftp.getLastMessage());
         ftp.issueCommand("RCMD CALL HACK400B/rcmdsetup");
         txtS.append(newline + ftp.getLastMessage());
         ftp.put("jshell3.class", "/home/hack400b/jshell3.class");
         txtS.append(newline + ftp.getLastMessage());
-
-        //ftp.put("./lib/jshell3.class", "/home/hack400b/lib/commons-net-3.6.jar");
-        //ftp.put("./lib/jopt-simple-5.0.3.jar", "/home/hack400b/lib/jopt-simple-5.0.3.jar");
         ftp.put("./lib/jt400.jar", "/home/hack400b/lib/jt400.jar");
         txtS.append(newline + ftp.getLastMessage());
         ftp.setDataTransferType(0);
@@ -109,38 +101,27 @@ public class ibmci3 extends JFrame implements ActionListener {
         txtS.append(newline + "FTP Connected succesfully to : " + ftp.getServer() + " on port: " + ftp.getPort());
 
     }
+    //Delete and Disconnect
     public void exitFTP(AS400FTP ftp) throws UnknownHostException, IOException {
-
-        //       AS400 system = new AS400();
-        //       AS400FTP   ftp    = new AS400FTP(system);
-
         ftp.connect();
         System.out.println(ftp.getLastMessage());
-
         ftp.issueCommand("RCMD CALL HACK400B/RCMDEXIT");
         txtS.setText(ftp.getLastMessage() );
         ftp.issueCommand("RCMD DLTLIB HACK400B");
         txtS.append(newline + ftp.getLastMessage());
         ftp.disconnect();
         txtS.append(newline + ftp.getLastMessage() + newline + "  FTP Disconnected");
-
-        // ftp.issueCommand("RCMD STRQSH CMD('mkdir /home/hack400c')");
     }
-
-
+    //Process Display Command
     public void processInformation(AS400FTP ftp) throws UnknownHostException, IOException {
-
         System.out.println("Proess Information Clicked");
         writeToFile(txtCommand.getText());
         txtS.append(newline);
         System.out.println(ftp.getLastMessage());
-
         //upload command box to ascii
         ftp.put("ftpinput.txt", "/home/hack400b/ftpinput.txt");
-
-        //execute rcmd2jshll
+        //execute RCMD2QSHL CLP
         ftp.issueCommand("RCMD CALL HACK400B/RCMD2QSHL");
-
         //download ascii output file
         ftp.get("/home/hack400b/ftpoutput.txt", "ftpoutput.txt");
 
@@ -157,17 +138,16 @@ public class ibmci3 extends JFrame implements ActionListener {
             try { buff.close(); } catch (Exception ex) { }
         }
     }
+    //Process Jshell Command
     public void processInformationJshl(AS400FTP ftp) throws UnknownHostException, IOException {
-
         System.out.println("Process Information Clicked");
         writeToFile(txtCommand.getText());
-
         System.out.println(ftp.getLastMessage());
 
         //upload command box to ascii
         ftp.put("ftpinput.txt", "/home/hack400b/ftpinput.txt");
 
-        //execute rcmd2jshll
+        //execute RCMD2JSHLI
         ftp.issueCommand("RCMD CALL HACK400B/RCMD2JSHLI");
 
         //download ascii output file
@@ -188,21 +168,8 @@ public class ibmci3 extends JFrame implements ActionListener {
         txtS.append(newline);
     }
 
-
-
-    public void showServerReply(FTPClient ftpClient) {
-        String[] replies = ftpClient.getReplyStrings();
-        if (replies != null && replies.length > 0) {
-            for (String aReply : replies) {
-              //  System.out.println("SERVER: " + aReply);
-                txtS.append("SERVER: " + aReply + newline);
-            }
-        }
-    }
-
-
+    //method to write desired command to ftpinput.txt on AS/400 for later processing
     private void writeToFile(String list) throws IOException{
-///
         File f = new File("ftpinput.txt");
         System.out.println(f);
         FileWriter fw = new FileWriter(f,false);
@@ -210,7 +177,6 @@ public class ibmci3 extends JFrame implements ActionListener {
         try{
             BufferedWriter bw = new BufferedWriter(fw);
             System.out.println(bw);
-           // bw.newLine();
             bw.write(list);
             bw.flush();
             bw.close();
@@ -218,17 +184,14 @@ public class ibmci3 extends JFrame implements ActionListener {
         catch(Exception e){
             System.out.println(e);
         }
-        ///
     }
+    //set up Java GUI window
     public ibmci3() {
-
 
         this.setTitle("AS400 IBM FTP Command Interpreter");
         this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
-
-
 
         //add labels and text boxes
         txtPort = new JTextField();
@@ -240,8 +203,6 @@ public class ibmci3 extends JFrame implements ActionListener {
         lblPort.setBounds(10, 10, 90, 21);
         add(lblPort);
 
-
-
         lblCommand = new JLabel("Command: ");
         lblCommand.setBounds(10, 135, 90, 21);
         add(lblCommand);
@@ -250,15 +211,12 @@ public class ibmci3 extends JFrame implements ActionListener {
         txtCommand.setBounds(105, 135, 550, 40);
         add(txtCommand);
 
-
-
         btnConnect = new JButton("Initiate FTP Connection");
         btnConnect.setForeground(Color.GREEN);
         btnConnect.setBackground(Color.black);
         btnConnect.setBounds(10, 40, 200, 40);
         btnConnect.addActionListener(this);
         add(btnConnect);
-
 
         btnProcess = new JButton("Process Display Command");
         btnProcess.setBounds(10, 185, 250, 40);
@@ -267,44 +225,24 @@ public class ibmci3 extends JFrame implements ActionListener {
 
         btnProcessJshl = new JButton("<HTML><i>Process Jshell Command</i></HTML>");
         btnProcessJshl.setBounds(400, 185, 250, 40);
-
         btnProcessJshl.addActionListener(this);
         add(btnProcessJshl);
 
-
         btnExit = new JButton("Delete and Disconnect");
         btnExit.setForeground(Color.RED);
-                btnExit.setBounds(10, 85, 200, 40);
+        btnExit.setBounds(10, 85, 200, 40);
         btnExit.addActionListener(this);
         add(btnExit);
 
-
-
         txtS = new JTextArea();
-
-        //txtS.setBounds(10, 245, 765, 500);
-
         scrollS = new JScrollPane(txtS);
         scrollS.setBounds(10, 245, 765, 300);
-
-
         add(scrollS);
 
-        //set testing values
-
-
-
         this.setVisible(true);
-
     }
-
+    //main
     public static void main(String[] args) {
-
-
         new ibmci3();
     }
-
-
-
-
 }
